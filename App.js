e9d737e4c87cb919386bd0b57a48bffc00ad221a
veronikanezdraj <veronikanezdraj@gmail.com>
2023-10-06 16:34:15 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+
+export default function App() {
+  return <StatusBar style="light" />;
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
